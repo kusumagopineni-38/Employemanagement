3d11792d4fa202f4356deae556061fb21a5c840a
root <root@ip-172-31-36-61.ec2.internal>
2026-08-18 17:20:02 +0000

    Fix unused Link import

--- employee-management/src/Pages/EmployeeDetails.tsx
+++ employee-management/src/Pages/EmployeeDetails.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { useNavigate, useParams, Link } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 const EmployeeDetails = () => {
